cd416e5a437b5909e8d775fc0d077aea7a765892
yxrs1 <129756501+yxrs1@users.noreply.github.com>
2023-07-03 17:23:30 +0800

    增加了超链接和一些菜单的子标题

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -5,21 +5,48 @@ import styles from './styles.module.css';
 const FeatureList = [
   {
     title: 'FAQ',
+   
     Svg: require('@site/static/img/FAQ.svg').default,
+    
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        <div style={{ textAlign: 'left' }}>
+          <a href="http://localhost:3000/my-website/docs/faq/">1.What is Max-Optics？</a>{"\n"}
+        </div>
+        {/* <a href="http://localhost:3000/my-website/docs/faq/">1.What is Max-Optics？</a>{"\n"} */}
+        <div style={{ textAlign: 'left' }}>
+          <a href="http://localhost:3000/my-website/docs/faq/faq1">2.How to install Max-Optics？</a>
+        </div>
+       
       </>
     ),
+    
+    
   },
   {
     title: 'Tutorials',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        <div style={{ textAlign: 'left' }}>
+          <a href="http://localhost:3000/docs\tutorial\mode\fde">1.what is FDE?</a>
+        </div>
+  
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\mode\fdtd">2.what is FDTD?</a>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\mode\eme">3.what is EME?</a>
+        </div>
+
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\material">4.how to set material?</a>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\t1">5.how to simulation?</a>
+        </div>
+       
+        
       </>
     ),
   },
@@ -29,8 +56,25 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        
+        <div style={{ textAlign: 'left' }}>
+          <a href="http://localhost:3000/docs\tutorial\mode\fde">1.如何调出所用命令？（待）</a>
+        </div>
+  
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\mode\fdtd">2.做一个例子，从mo.sdk.doc到knowledge base的item?（待）</a>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\mode\eme">3.用GUI创建?（待）</a>
+        </div>
+
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\material">4.用SDK创建？（待）</a>
+        </div>
+        
+       
+        
+      
       </>
     ),
   },
@@ -40,8 +84,38 @@ const FeatureList = [
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        <div style={{ textAlign: 'left' }}>
+          <a href="http://localhost:3000/docs\tutorial\mode\fde">1.PIC</a>
+        </div>
+  
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\mode\fdtd">2.Grating</a>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\mode\eme">3.Metamaterial</a>
+        </div>
+
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\material">4.PD(by GUI)</a>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\t1">5.PD(by SDK)</a>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\mode\eme">6.MOD(by GUI)</a>
+        </div>
+
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\material">7.MOD(by SDK)</a>
+        </div>
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\t1">8.Lasers</a>
+        </div>
+        
+        <div style={{ textAlign: 'left' }}>
+        <a href="http://localhost:3000/docs\tutorial\t1">9.Optical scattering and far-field radiation</a>
+        </div>
+       
       </>
     ),
   },
