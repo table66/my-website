10efcee502a566957afb07cced6bb086100ad8dc
table66 <64113188+table66@users.noreply.github.com>
2023-07-04 09:44:02 +0800

    run

--- .docusaurus/docusaurus.config.mjs
+++ .docusaurus/docusaurus.config.mjs
@@ -25,7 +25,7 @@ export default {
       "classic",
       {
         "docs": {
-          "sidebarPath": "/Users/zhuyuanji/Documents/my-website/sidebars.js",
+          "sidebarPath": "E:\\研究生\\曼光\\website\\right\\my-website\\sidebars.js",
           "editUrl": "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
         },
         "blog": {
@@ -33,7 +33,7 @@ export default {
           "editUrl": "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/"
         },
         "theme": {
-          "customCss": "/Users/zhuyuanji/Documents/my-website/src/css/custom.css"
+          "customCss": "E:\\研究生\\曼光\\website\\right\\my-website\\src\\css\\custom.css"
         }
       }
     ]
