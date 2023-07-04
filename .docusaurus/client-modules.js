9efcf090ee46587ca2393099ab6771dc095866f7
table66 <64113188+table66@users.noreply.github.com>
2023-07-04 14:14:15 +0800

    del node_Module and .docusaurus

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require('E:\\研究生\\曼光\\website\\right\\my-website\\node_modules\\infima\\dist\\css\\default\\default.css'),
-  require('E:\\研究生\\曼光\\website\\right\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages'),
-  require('E:\\研究生\\曼光\\website\\right\\my-website\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress'),
-  require('E:\\研究生\\曼光\\website\\right\\my-website\\src\\css\\custom.css'),
-];
